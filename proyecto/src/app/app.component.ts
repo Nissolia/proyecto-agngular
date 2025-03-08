36fa176ce3113659bac6bd3cb3341e032137704c
Noelia\Portatil_Noelia <nissoliadesing@gmail.com>
2025-03-08 15:13:18 +0100

    Prueba insercción barra de menu

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -10,7 +10,17 @@ declare var $: any;
     styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  title = 'proyecto';
     ngOnInit(): void {
         $('[data-bs-toggle="popover"]').popover();
+    }
+
+
+    opciones = [
+      "Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5"
+    ];
+  
+    presion(op: number) {
+      alert("Se informa que se presiono la opcion " + op);
     }
 }
