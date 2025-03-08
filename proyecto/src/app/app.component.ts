947ee1af54bc8d8492e29587a1b100be8f5fcead
Noelia\Portatil_Noelia <nissoliadesing@gmail.com>
2025-03-08 14:04:08 +0100

    cambio en style e insercción de logo

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+declare var $: any;
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'proyecto';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        $('[data-bs-toggle="popover"]').popover();
+    }
 }
